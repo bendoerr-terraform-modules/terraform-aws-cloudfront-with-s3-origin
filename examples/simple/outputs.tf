93e6257b761a74bfd5826c85124425adcdef50ef
Ben Doerr <craftsman@bendoerr.me>
2024-08-20 15:31:47 -0400

    Update examples/simple/outputs.tf
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -1,24 +1,24 @@
 output "s3_bucket_id" {
   value       = module.cloudfront_with_s3_origin.s3_bucket_id
-  description = "TODO"
+  description = "The ID of the S3 bucket used for the CloudFront origin."
 }
 
 output "s3_bucket_arn" {
   value       = module.cloudfront_with_s3_origin.s3_bucket_arn
-  description = "TODO"
+  description = "The ARN of the S3 bucket used for the CloudFront origin."
 }
 
 output "cloudfront_distribution_id" {
   value       = module.cloudfront_with_s3_origin.cloudfront_distribution_id
-  description = "TODO"
+  description = "The ID of the CloudFront distribution."
 }
 
 output "cloudfront_distribution_arn" {
   value       = module.cloudfront_with_s3_origin.cloudfront_distribution_arn
-  description = "TODO"
+  description = "The ARN of the CloudFront distribution."
 }
 
 output "cloudfront_distribution_domain_name" {
   value       = module.cloudfront_with_s3_origin.cloudfront_distribution_domain_name
-  description = "TODO"
+  description = "The domain name of the CloudFront distribution."
 }
